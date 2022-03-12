5eaf897beeefcbbb85986279cd2726ef12daa2eb
SonalChadha <sonalchadha25@gmail.com>
2022-03-12 12:45:32 -0600

    config

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PlayerList from "./components/PlayerList";
 import Navbar from "./components/Navbar";
 import UpdateRedPlayer from "./components/UpdateRedPlayer";
 import UpdateGreenPlayer from "./components/UpdateGreenPlayer";
-import GameScreen from "./components/GameScreen";
+//import GameScreen from "./components/GameScreen";
 import SpashScreen from "./components/SplashScreen";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route index element={<SpashScreen />} />
           <Route path="/playerEntryScreen" element={<PlayerList />} />
-          <Route path="/gameScreen" element={<GameScreen />} />
+          //<Route path="/gameScreen" element={<GameScreen />} />
           <Route path="/editRedPlayer/:id" element={<UpdateRedPlayer />} />
           <Route path="/editGreenPlayer/:id" element={<UpdateGreenPlayer />} />
         </Routes>
