283cff548d5ae2adc4a7df72ba5584bcff1d0626
Sonal Chadha <sonalchadha25@gmail.com>
2022-04-15 13:18:39 -0500

    port binding issue fix

--- src/services/GreenPlayerService.js
+++ src/services/GreenPlayerService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const GREEN_PLAYER_API_BASE_URL = "http://127.0.0.1:7500/api/v1/greenTeam";
+const GREEN_PLAYER_API_BASE_URL = "http://127.0.0.1:8080/api/v1/greenTeam";
 
 class GreenPlayerService {
   savePlayer(player) {
